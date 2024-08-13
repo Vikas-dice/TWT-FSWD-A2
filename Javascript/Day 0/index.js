28c337c4efb0df73439742ae93c0aaf32531433e
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-08-13 11:14:16 +0530

    code pushed for js day 0

---
+++ Javascript/Day 0/index.js
@@ -0,0 +1,76 @@
+// single line comment
+
+/*
+output
+variables
+
+maths - arithmatic 
+
++-* %/
+
+
+
+
+
+
+*/
+
+console.log("hello world !");
+console.log("hey");
+console.log("vikas");
+console.log("vikas chaudhary"); console.log("dice");
+
+process.stdout.write("full stack");
+process.stdout.write("mern stack");
+
+
+let age=28;
+let name1="vikas";
+let ismarried=false;
+let salary=10000;
+
+console.log(age);
+// console.log(name);
+console.log(ismarried);
+console.log(salary);
+
+
+console.log("--------------------------------");
+
+
+process.stdout.write(age);
+process.stdout.write(ismarried);
+process.stdout.write(salary);
+process.stdout.write(name1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/Day 0/index2.js
@@ -0,0 +1,112 @@
+console.log(10+5);
+console.log(5-10);
+console.log(5*10);
+console.log(10/5);
+console.log(Math.floor(11/5));
+console.log(Math.ceil(11/5));
+console.log(11%6);
+
+
+// RELATIONAL OPER > < >= <= == !=
+console.log(10>5);
+console.log(10<5);
+console.log(10==5);
+console.log(10!=5);
+console.log(10>=20);
+console.log(10>=10);
+console.log(10<=15);
+console.log(10<=10);
+
+
+
+
+/*
+10<15 true
+10>10 false
+10=10 true
+
+
+*/
+
+/*
+10>20 fALSE
+10=20 False
+
+
+
+*/
+
+/*
+
+logical op  and or not
+
+and && hindi m guna krna 
+or || hindi m jodnA 
+
+
+
+*/
+
+console.log("----------------------");
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+console.log(false || true);
+
+
+console.log("----------------------");
+
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(false && true);
+
+
+console.log(!true);
+console.log(!false);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+1 && 1
+1*1=1=trye
+
+*/
+
+
+
+
+
+
+/*
+1 || 0
+1+0=1 true
+1 || 1
+1+ 1= 2 true
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
