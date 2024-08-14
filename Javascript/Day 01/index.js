08f4bef60ad5a37751c1476f20a1d0fba6efae13
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-08-14 11:15:54 +0530

    code pushed for js day 01 -conditionals switch ternary operator

---
+++ Javascript/Day 01/index.js
@@ -0,0 +1,97 @@
+
+
+
+// let age =17;
+
+
+// if(age>18){
+//     console.log("you can vote");
+// }
+// console.log("m if block m nahiu fass paya islye l10 m aagya ");
+
+/*
+l4 ram=age=19
+l7 age>18 19>18 true
+l8 you can vote 
+
+l4 ram=age=17
+l7 age>18 17>18 false
+
+
+l10 
+
+
+
+
+*/
+
+// let age =20;
+// if(age<20){
+//     console.log("you are under 20");   
+// }
+// if(age==18){
+//     console.log("i am 18");   
+// }
+// if(age>18){
+//     console.log("i am over 18");
+    
+// }
+/*
+l28 ram=age=20
+l29 age<20 20<20 false
+l32 age==18 20==28 false
+l35 age>18 20>18 true
+l36 i am over 18
+
+
+*/
+
+
+// let age=23;
+// if(){
+//     console.log("i am developer ");
+    
+// }
+
+
+
+
+// let age =17;
+// if(age>18){
+//     console.log("you can vote");
+    
+// }else{
+//     console.log("you cant vote");
+    
+// }
+
+
+let age=33;
+
+if(age<15){ //45<15 fa;se
+    console.log("go to school");
+    
+}else if(age>15 && age<18){ //45>15 && 45<18
+        console.log("go to college ");
+        
+    }else if(age>18 && age<24){ 
+            console.log("go tow work");
+            
+        }else if(age>24 && age<34){ //45>24 true && 45<34
+                console.log("go n get merry");
+                
+            }else if(age>34 && age<40){ //45>34 true && 45<40
+                    console.log(" go take your kids to school");
+                    
+                }else if(age>40 && age<60){ //45>40 true && 45<60 true
+                        console.log(" spend some quality time with your family");
+                        
+                    }else if(age>60){
+                            console.log("go to mandir");
+                            
+                        }else{
+                            console.log(" rip");
+                            
+                        }
+                    
+                
---
+++ Javascript/Day 01/questions.js
@@ -0,0 +1,164 @@
+
+
+
+/*
+marks >90 grade a
+marks 80> <90 gradde b
+marks 70> <80 grade c
+marks>60 <70 grade d
+marks>50 <60 frade e
+fail
+
+
+
+
+
+
+
+
+*/
+
+let marks =78;
+if(marks>90){
+    console.log("grade a");
+    
+}else if(marks>80 && marks<90){
+    console.log("grade b");
+    
+}else if(marks>70 && marks<80){
+    console.log("grade c");
+    
+}else if(marks>60 && marks<70){
+    console.log("grade d");
+    
+}else if(marks>50 && marks<60){
+    console.log("grade e");
+    
+}else{
+    console.log("fail");
+    
+}
+
+
+/*
+number oddd ? even 
+
+odd = 
+
+
+
+
+*/
+
+// let num=780;
+// if(num%2==0){
+//     console.log("even number",num);
+    
+// }else{
+//     console.log("odd number ",num);
+    
+// }
+
+/*
+
+
+number test - 
+3 fizz
+5 buzzz
+3&5 fizzbuzz
+
+
+
+*/
+
+// let num=21;
+// if(num%15==0){
+//     console.log("fizzbuzz");
+    
+// }else if(num%3==0){
+//     console.log("fizz");
+    
+// }else if(num%5==0){
+//     console.log("buzz");
+    
+// }else{
+//     console.log("invalid number ");
+    
+// }
+
+
+
+//switch case 
+
+// let age=20;
+// switch(age){
+//     case 18:
+//         console.log(" i am 18");
+//         break;
+//     case 19:
+//         console.log("i am 19");
+//         break;
+        
+        
+//     case 20:
+//         console.log("i am 20");
+//         break;
+//     case 21:
+//         console.log("i am 21");
+//         break;
+//     case 22:
+//         console.log("i am 22");
+//         break;
+//     default:
+//         console.log("i am under age ");
+        
+        
+        
+        
+// }
+// console.log("break ki vjah se switch k bahar fenk diaa");
+
+
+// let day="Wednesday";
+// switch(day){
+//     case "monday":
+//         console.log("i am monday");
+//         break;
+//     case "tuesday":
+//         console.log("i am tuesday");
+//         break;
+//     case "wednesday":
+//         console.log("i am wednesday");
+//         break;
+//     case "thursday":
+//         console.log("i am thursday");
+//         break;
+//     case "friday":
+//         console.log("i am friday");
+//         break;
+//     case "saturday":
+//         console.log("i am saturday");
+//         break;
+//     case "sunday":
+//         console.log("i am sunday");
+//         break;
+//     default:
+//         console.log("invalid day");
+        
+        
+        
+        
+        
+        
+        
+        
+// }
+
+
+//TERNARY OPERATOR  ? :
+
+
+let age=16;
+
+age>18?console.log("i can vote"):console.log("i cant vote");
+
