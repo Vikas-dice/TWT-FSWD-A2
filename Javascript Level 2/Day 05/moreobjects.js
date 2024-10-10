5fc5d57fd19d260a8d16fa3466b9e592909217ae
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-10-10 11:24:40 +0530

    code pushed for browser apis

---
+++ Javascript Level 2/Day 05/moreobjects.js
@@ -0,0 +1,142 @@
+
+
+// /*
+// objects 
+// class - faculties
+// admin 
+// operations 
+// aux staff 
+
+
+
+
+// */
+
+
+// let emp1={
+//     name:"vikas",
+//     age:27,
+//     salary:10000,
+//     dept:"faculty"
+// }
+
+// let emp2={
+//     name:"saviour",
+//     age:29,
+//     salary:20000,
+//     dept:"operations"
+// }
+
+// let emp3={
+//     name:"manas",
+//     age:18,
+//     salary:30000,
+//     dept:"aux"
+// }
+
+//cs - dry
+//jugaad - values - new new object - dry 
+
+//constructor - new thing - new -object 
+
+//function cons , class cons 
+
+//function cons - es5 
+
+//class cons - es6 
+
+
+//syntax 
+
+// function 
+
+// let arr=[10,20,30];
+// console.log(arr.__proto__);
+
+
+
+// function Employees(name,age,salary,dept){
+    
+
+//     this.name=name;
+//     this.age=age;
+//     this.salary=salary;
+//     this.dept=dept;
+
+
+   
+
+
+
+
+
+
+// }
+
+// let employee1=new Employees("vikas",27,10000,"faculty");
+// let employee2= new Employees("saviour",29,20000,"operations")
+// let employee3= new Employees("manas",18,30000,"aux")
+// console.log(employee1);
+// console.log(employee2);
+// console.log(employee3);
+
+
+
+
+
+// function Employee(naam,umar,tankhuwa,dept,task){
+
+//     this.name=naam;
+//     this.age=umar;
+//     this.salary=tankhuwa;
+//     this.dept=dept;
+//     this.task=task;
+
+
+
+
+
+
+// }
+
+// let emp1=new Employee("vikas",27,10000,"faculty")
+// let emp2=new Employee("manas",18,20000,"aux")
+// let emp3= new Employee("sultan",30,15000,"admin",function(){
+//     console.log(" i am from admin dept ");
+//     return 100;
+    
+// })
+
+// console.log(emp1==emp2);
+
+
+// // console.log(emp1);
+// console.log(emp3.task());
+
+
+
+// function Employee(name,age,salary,dept){
+
+
+//     this.name="vikas";
+//     this.age=27;
+//     this.salary=10000,
+//     this.dept=dept;
+// }
+
+
+
+
+// let emp1=new Employee("rohan",20,5000,"tech");
+// // let emp2=new Employee();
+// // let emp3=new Employee();
+// // let emp4=new Employee();
+
+// // console.log(emp1,emp2,emp3,emp4);
+// console.log(emp1);
+
+
+
+
+//task - dynamic object - 4 keys - 1 key - object
+
---
+++ Javascript Level 2/Day 06/BrowserAPI.js
@@ -0,0 +1,62 @@
+
+
+/*
+settimeout 
+
+
+
+*/
+
+// setTimeout("callback func","timer")
+
+
+// console.log("dice ")
+// setTimeout(function(){
+//     console.log("vikas")
+
+// },4000)
+
+// console.log("academy")
+
+
+// setTimeout(()=>{
+//     console.log("vikas")
+
+// },5000)
+
+//setinterval
+
+
+// setInterval("callback func","timer")
+
+// setInterval(function(){
+//     console.log("vikas")
+
+// },2000)
+
+// setInterval(() => {
+//     console.log("vikas")
+    
+// }, 1000);
+
+
+// setTimeout(()=>{
+//     setInterval(()=>{
+//         console.log("vikas")
+
+//     },1000)
+
+// },2000)
+
+
+//clock - hr , min , sec 
+
+/*
+
+60sec+ - min +1
+min>60 - hr +1
+
+
+
+
+*/
