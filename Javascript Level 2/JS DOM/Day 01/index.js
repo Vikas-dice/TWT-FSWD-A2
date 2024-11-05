ea08871f8d7ea2449c6f666d5442ae4b3aea8817
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-11-05 11:18:26 +0530

    dom js day 1

---
+++ Javascript Level 2/JS DOM/Day 01/index.js
@@ -0,0 +1,41 @@
+
+// console.log("vikas");
+
+
+console.log("element selection")
+
+/*
+1- getelemntbyid
+2-getelebyclassname
+3-getlemtagname
+
+
+
+*/
+
+// let heading=document.getElementsByName('h1')
+// console.log(heading)
+
+// let heading=document.querySelectorAll('h1')
+// console.log(heading)
+
+// let heading=document.getElementsByTagName('h1')
+// console.log(heading)
+
+// let headingone=document.getElementById('heading1')
+// console.log(headingone)
+
+
+// let headingone=document.getElementById('#heading2')
+// console.log(headingone)
+
+
+// let headingpone=document.querySelector('h1')
+
+// console.log(headingpone)
+
+// console.log(document)
+
+
+let headingone=document.getElementById("heading1")
+console.log(headingone)
